089564bd244b9d96c6619b96d9eabac542710e60
fiqriachmada <achmada1998@gmail.com>
2022-07-06 15:03:25 +0700

    New Features

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
